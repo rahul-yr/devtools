2459458446a7acf45b8a821d18c77dbc79552efc
Rahul Reddy <yarragodularahulreddy@gmail.com>
2022-07-29 13:52:02 +0530

    Create ubuntu-virtual-desktop.sh

---
+++ Ubuntu_virtual_desktop/ubuntu-virtual-desktop.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Official source for installation
+echo "Official Source : https://ubuntu.com/blog/launch-ubuntu-desktop-on-google-cloud"
+
+echo "==============================================================="
+echo "Installing dependencies"
+
+sudo apt update
+sudo apt install --assume-yes wget tasksel
+
+echo "Installing Google Chrome Remote Desktop"
+# install chrome remote desktop
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb
+
+echo "Installing Ubuntu Desktop Minimal"
+# install ubuntu desktop minimal GUI
+sudo tasksel install ubuntu-desktop-minimal
+sudo bash -c ‘echo “exec /etc/X11/Xsession /usr/bin/gnome-session” > /etc/chrome-remote-desktop-session’
+
+echo "==============================================================="
+echo "Done installing dependencies"
+echo "Next Steps : "
+echo "First reboot the machine using 'sudo reboot' command"
+echo "Then ssh into the machine"
+echo "Copy and paste chrome remote desktop authentication details"
+echo "Then add the 6 digit login password to the machine"
+echo "sudo systemctl status chrome-remote-desktop@$USER"
+echo "use the above command to check if chrome remote desktop is running"
+
+# ==================================================================
+# Helper commands
+# sudo systemctl status chrome-remote-desktop@$USER
